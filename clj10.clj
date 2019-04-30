e313fc6f4157811eb4e7bd6f7389e09b48d84bb2
Jonatas Vieira <jonatas.vieira@la.logicalis.com>
2019-04-30 00:29:49 -0300

    Trying to solve 10.2 exercise to count number of occurrences in a given word.

--- clj10.clj
+++ clj10.clj
@@ -3,7 +3,6 @@
 ;vetor de pessoas, sendo cada pessoa um mapa contendo :nome e :idade.
 
 
-(def pessoas '({:nome "joao" :idade 15} {:nome "maria" :idade 30} {:nome "jose" :idade 40} {:nome "gustavo" :idade 15}))
 
 ; (defn media-idade [pessoas]
 ;     (def num-pessoas (count pessoas))
@@ -20,6 +19,11 @@
 ;         0
 ;         (recur (+ (:idade (first v)) (rest v))))))
 
-(defn media-idade[pessoas]
-    (reduce + (fn[pessoa] (:idade pessoa)) pessoas))
+    (def pessoas '({:nome "joao" :idade 15} {:nome "maria" :idade 30} {:nome "jose" :idade 40} {:nome "gustavo" :idade 15}))
+    (defn media-idade[pessoas]
+        (/ (reduce + (map (fn[p] (:idade p)) pessoas)) (count pessoas)))
+
+;10.2
+(defn conta-letras[palavra]
+    (map (fn [letra] (conj '() :letra (inc 1))) palavra))
    
